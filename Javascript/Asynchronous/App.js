29403f6a869ed2d405d125adaa12137a17f1237e
Emaan Fatima <alizbat14@gmail.com>
2026-02-12 22:36:25 +0500

    Add new logics

--- Javascript/Asynchronous/App.js
+++ Javascript/Asynchronous/App.js
@@ -23,11 +23,16 @@
 // calculator(2,5,sum)
 // Call back hell
 
-function getData(dataID){
+function getData(dataID,getNextData){
     setTimeout(()=>{
   console.log("Data",dataID)
+  if (getNextData){
+    getNextData();
+  }
     },2000)
 }
-getData(1,()=>{
-    getData(2)
+getData(1,()=> {
+    getData((2),()=>{
+        getData(3);
+    });
 })
